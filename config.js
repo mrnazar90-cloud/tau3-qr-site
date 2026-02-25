d60dbdbb350b83089a0e6e6720723e199c64f762
mrnazar90-cloud <mr.nazar90@gmail.com>
2026-02-25 16:28:33 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Конфигурация
 // 1) Google Apps Script Web App URL-ін осында қойыңыз (Deploy -> Web app URL)
 // Мысал: const SUBMIT_URL = "https://script.google.com/macros/s/AKfycbxERavhBc1v2QgCFgNDkpyPYCwv-ntxoMbSVv2wdi2YXVIr-NJTUJC7jGVGpmQvxjw/exec";
-const SUBMIT_URL = "";
+const SUBMIT_URL = "https://script.google.com/macros/s/AKfycbxERavhBc1v2QgCFgNDkpyPYCwv-ntxoMbSVv2wdi2YXVIr-NJTUJC7jGVGpmQvxjw/exec";
 
 // 2) (Қаласаңыз) байланыс үшін мұғалімнің email/telegram
 const TEACHER_CONTACT = "Мұғалімге байланыс: ...";
